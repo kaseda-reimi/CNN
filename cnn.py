725337db053c36e6beed2f42dc2a953c79b73270
加世田怜美 <kasedareibi@MacBook-Air.local>
2021-07-09 07:59:33 +0900

    cnn.py

--- cnn.py
+++ cnn.py
@@ -3,8 +3,8 @@ from tensorflow.keras.layers import Conv2D, MaxPool2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.datasets import cifar10
-import os
+from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
 
 classes = 10
 lr = 0.001
@@ -12,36 +12,34 @@ batch_size = 128
 epochs = 10
 
 def main():
-    (X_train, y_train),(X_test, y_test) = cifar10.load_data()
-    # floatに型変換
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-    # 各画素値を正規化
-    X_train /= 255.0
-    X_test /= 255.0
-
-    Y_train = to_categorical(y_train, classes)
-    Y_test = to_categorical(y_test, classes)
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train  = x_train.reshape(60000, 28, 28, 1)
+    x_test   = x_test.reshape(10000, 28, 28, 1)
+    x_train  = x_train.astype('float32')
+    x_test   = x_test.astype('float32')
+    x_train /= 255
+    x_test  /= 255
+    y_train  = to_categorical(y_train, classes)
+    y_test   = to_categorical(y_test, classes)
 
     # モデルの定義
     model = Sequential()
 
-    model.add(Conv2D(32,kernel_size=3,input_shape=(32,32,3)))
-    model.add(Activation("relu"))
-
-    model.add(Conv2D(32,kernel_size=3))
+    model.add(Conv2D(32,(3, 3),input_shape=(28,28,1)))
     model.add(Activation("relu"))
     model.add(MaxPool2D(pool_size=(2,2)))
 
-    model.add(Conv2D(64,kernel_size=3))
+    model.add(Conv2D(64,(3, 3)))
     model.add(Activation("relu"))
     model.add(MaxPool2D(pool_size=(2,2)))
+
+    model.add(Conv2D(64,(3, 3)))
+    model.add(Activation("relu"))
 
     model.add(Flatten())
-    model.add(Dense(1024))
+    model.add(Dense(64))
     model.add(Activation("relu"))
     #model.add(Dropout(1.0))
-
     model.add(Dense(classes, activation=("softmax")))
 
     adam = Adam(learning_rate=lr)
@@ -51,8 +49,24 @@ def main():
     )
 
     history = model.fit(
-        X_train, Y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_split=0.1
+        x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_split=0.1
     )
+
+    score = model.evaluate(x_test, y_test, verbose=1)
+    print()
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+    loss     = history.history['loss']
+    val_loss = history.history['val_loss']
+    nb_epoch = len(loss)
+    plt.plot(range(nb_epoch), loss,     marker='.', label='loss')
+    plt.plot(range(nb_epoch), val_loss, marker='.', label='val_loss')
+    plt.legend(loc='best', fontsize=10)
+    plt.grid()
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.savefig("cnn_learning.png")
 
 if __name__ == '__main__':
     main()
