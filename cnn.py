3ea8c408a8c34bf620a5a67d39b76e2c2841b287
加世田怜美 <kasedareibi@MacBook-Air.local>
2021-07-09 04:46:32 +0900

    cnn.py

--- cnn.py
+++ cnn.py
@@ -5,10 +5,10 @@ from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten
 from tensorflow.keras.utils import plot_model, to_categorical
 from tensorflow.keras.datasets import cifar10
 
-nb_classes = 10
+classes = 10
 lr = 0.001
-batch_size = 32
-nb_epoch = 100
+batch_size = 128
+epochs = 10
 
 def main():
     (X_train, y_train),(X_test, y_test) = cifar10.load_data()
@@ -19,34 +19,38 @@ def main():
     X_train /= 255.0
     X_test /= 255.0
 
-    Y_train = to_categorical(y_train, nb_classes)
-    Y_test = to_categorical(y_test, nb_classes)
+    Y_train = to_categorical(y_train, classes)
+    Y_test = to_categorical(y_test, classes)
 
     # モデルの定義
     model = Sequential()
 
-    model.add(Conv2D(32,kernel_size=3,activation="relu",input_shape=(32,32,3)))
-    model.add(Conv2D(32,kernel_size=3,activation="relu"))
+    model.add(Conv2D(32,kernel_size=3,input_shape=(32,32,3)))
+    model.add(Activation("relu"))
+
+    model.add(Conv2D(32,kernel_size=3))
+    model.add(Activation("relu"))
     model.add(MaxPool2D(pool_size=(2,2)))
 
-    model.add(Conv2D(64,kernel_size=3,activation="relu"))
+    model.add(Conv2D(64,kernel_size=3))
+    model.add(Activation("relu"))
     model.add(MaxPool2D(pool_size=(2,2)))
 
     model.add(Flatten())
-    model.add(Dense(1024,activation="relu"))
-    model.add(Dropout(1.0))
+    model.add(Dense(1024))
+    model.add(Activation("relu"))
+    #model.add(Dropout(1.0))
 
-    model.add(Dense(nb_classes))
-    model.add(Activation("softmax"))
+    model.add(Dense(classes, activation=("softmax")))
 
-    optimizer = Adam(learning_rate=lr)
+    adam = Adam(learning_rate=lr)
 
     model.compile(
-        optimizer=optimizer, loss='categorical_crossentropy', metrics=["accuracy"]
+        optimizer=adam, loss='categorical_crossentropy', metrics=["accuracy"]
     )
 
     history = model.fit(
-        X_train, Y_train, batch_size=batch_size, epochs=nb_epoch, verbose=1, validation_split=0.1
+        X_train, Y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_split=0.1
         )
 
 if __name__ == '__main__':
