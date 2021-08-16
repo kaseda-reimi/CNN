392f5350a4b4f40796c985e5148726f24134b768
kaseda-reimi <kaseda-reim-xc@ynu.jp>
2021-08-16 09:05:59 +0900

    data_r.txt

--- function.py
+++ function.py
@@ -8,15 +8,18 @@ def get_data():
         l = f.read().split()
     data = [float(s) for s in l]
     data = np.array(data).reshape(-1, x_len*y_len+2)
-    structure = data[:,:x_len*y_len].reshape(-1, x_len*y_len)
+    structure = data[:,:x_len*y_len].reshape(-1, y_len, x_len)
     output = data[:,x_len*y_len:].reshape(-1, 2)
     output_ratio = np.zeros([output.shape[0],2])
     output_ratio[:,0] = 2 * np.log10(output[:,0]/output[:,1]) #消光比/10
     output_ratio[:,1] = 2 * np.log10(1/output[:,0]) #挿入損失/10
+    structure = np.concatenate([structure, np.flip(structure,1)])
+    output_ratio = np.concatenate([output_ratio, output_ratio])
     return structure, output_ratio
 
 if __name__ == '__main__':
     x, y = get_data()
-    print(x[0])
-    print(y[0])
+    print(x[37])
+    print(y[37])
+    print(x.shape[0])
     
