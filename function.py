1d6b84585755bc95f1bba14d5716a12a1499e7c1
kaseda-reimi <kaseda-reim-xc@ynu.jp>
2021-11-19 03:01:12 +0900

    functionc

--- function.py
+++ function.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from design import x_len, y_len
 
 def get_data():
@@ -22,6 +22,21 @@ def get_data():
     structure = np.concatenate([structure, np.flip(structure,1)])
     output_ratio = np.concatenate([output_ratio, output_ratio])
     return structure, output_ratio
+
+def get_data_half():
+    x_len = 20
+    path = os.getcwd()+'/data_half.txt'
+    with open (path) as f:
+        l = f.read().split()
+    data = [float(s) for s in l]
+    data = np.array(data).reshape(-1, x_len*y_len+2)
+    structure = data[:,:x_len*y_len].reshape(-1, y_len, x_len)
+    output = data[:,x_len*y_len:].reshape(-1, 2)
+    #output_ratio = output**2
+    output_ratio = np.zeros([output.shape[0],2])
+    output_ratio[:,0] = 2 * np.log10(output[:,0]/output[:,1]) #消光比/10
+    output_ratio[:,1] = 2 * np.log10(1/output[:,0]) #挿入損失/10
+    return structure, output, output_ratio
 
 def normalize(x):
     normalized_x= (x - np.amin(x)) / (np.amax(x) - np.amin(x))
@@ -51,41 +66,25 @@ def search_E_max(data):
             max_index = i
     return E_max, max_index
 
-def data_bunseki():
-    path = os.getcwd()+'/data_r2.txt'
-    with open (path) as f:
-        l = f.read().split()
-    data = [float(s) for s in l]
-    data = np.array(data).reshape(-1, x_len*y_len+2)
-    output = data[:,x_len*y_len:].reshape(-1, 2)
-    output_ratio = np.zeros([output.shape[0],2])
-    output_ratio[:,0] = 20 * np.log10(output[:,0]/output[:,1]) #訂正後消光比
-
-    path = os.getcwd()+'/data_r.txt'
-    with open (path) as f:
-        l = f.read().split()
-    data = [float(s) for s in l]
-    data = np.array(data).reshape(-1, x_len*y_len+2)
-    output = data[:,x_len*y_len:].reshape(-1, 2)
-    output_ratio[:,1] = 20 * np.log10(output[:,0]/output[:,1]) #訂正前消光比
-
-    return output_ratio
 
 
 
 
 if __name__ == '__main__':
-    output_ratio = data_bunseki()
-    #print(np.amax(output_ratio))
-    x = list(range(-3, 14, 1))
-    y = np.zeros([2,17])
-    for i in range(output_ratio.shape[0]):
-        a = output_ratio[i][0]//1 + 3
-        print(a)
+    input, output1 = get_data()
+    print(np.amin(output1))
+    print(np.amax(output1))
+    #print(input.shape)
+    #print(output1[1])
+    x = list(range(0, 10, 10))
+    y = np.zeros([2,10])
+    for i in range(output1.shape[0]):
+        a = output1[i][0]//0.1
         y[0][int(a)] += 1
-        b = output_ratio[i][1]//1 + 3
+        b = output1[i][1]//0.1
         y[1][int(b)] += 1
-    
-    plt.plot(x, y[0], marker="o", color = "red", linestyle = "--")
-    plt.plot(x, y[1], marker="o", color = "blue", linestyle = "--")
-    plt.savefig("data_hikaku.png")
+    print(y)
+
+    #plt.plot(x, y[0], marker="o", color = "red", linestyle = "--")
+    #plt.plot(x, y[1], marker="o", color = "blue", linestyle = "--")
+    #plt.savefig("data_hikaku.png")
