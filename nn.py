531dc2d91d055089d97cba22af29516a2a206b2e
kaseda-reimi <kaseda-reim-xc@ynu.jp>
2021-11-24 05:14:03 +0900

    nn.py

--- nn.py
+++ nn.py
@@ -33,7 +33,7 @@ def main():
     model.add(Dense(input_size/1, activation = "relu"))#120
     model.add(Dense(input_size/1, activation = "relu"))#80
     model.add(Dense(input_size/1, activation = "relu"))
-    model.add(Dense(input_size/2, activation = "relu"))
+    model.add(Dense(input_size/1, activation = "relu"))
     model.add(Dense(2, activation='linear'))
     
     model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['mae'])
