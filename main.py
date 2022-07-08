8614f9f4bbab6c844ebf81a708c9b2f77f2ba074
kaseda-reimi <kaseda-reim-xc@ynu.jp>
2022-07-08 09:21:27 +0900

    main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main():
     #評価
     model = load_model(model_path)
     perform = model.predict(design.reshape(1, -1))
-    eval = fc.evaluation(perform)
+    eval = fc.evaluation(perform[0])
     start = copy.copy(design)
     eval_start = copy.copy(eval)
     for i in range(epochs):
