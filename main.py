f18a81430c248c6cf04f5eef7ddce02253c60f46
kaseda-reimi <kaseda-reim-xc@ynu.jp>
2023-03-04 15:39:47 +0900

    main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
                 best_design = copy.deepcopy(neighbor)
         design = best_design
         eval = best_eval
-        history[i] = [i,best_eval]
+        history[i] = i,best_eval
         print(i, best_eval)
     
     print(start)
