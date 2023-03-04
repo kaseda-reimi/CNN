fb060dcf9c029ce95b22fe707804754ec1367c99
kaseda-reimi <kaseda-reim-xc@ynu.jp>
2023-03-04 13:27:30 +0900

    nn.py

--- nn.py
+++ nn.py
@@ -32,7 +32,7 @@ def main():
     model.add(Dense(input_size/1, activation = "relu"))
     model.add(Dense(input_size/1, activation = "relu"))
     model.add(Dense(input_size/1, activation = "relu"))
-    model.add(Dense(input_size/1, activation = "relu"))
+    model.add(Dense(input_size/2, activation = "relu"))
     #model.add(Dense(input_size/6, activation = "relu"))
     model.add(Dense(2, activation='sigmoid'))
     
